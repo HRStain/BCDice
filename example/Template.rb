61d56fc4120c315ca69f9819168ce5f635b08618
HRStain <97943658+HRStain@users.noreply.github.com>
2022-02-28 17:38:49 +0900

    Update Template.rb

--- example/Template.rb
+++ example/Template.rb
@@ -4,17 +4,21 @@ module BCDice
   module GameSystem
     class Template < Base
       # ゲームシステムの識別子
-      ID = "Template"
+      ID = "OnseTool"
 
       # ゲームシステム名
-      NAME = "ゲームシステム名"
+      NAME = "オンセツールTRPG"
 
       # ゲームシステム名の読みがな
-      SORT_KEY = "けえむしすてむめい"
+      SORT_KEY = "おんせつうるTRPG"
 
       HELP_MESSAGE = <<~TEXT
-        ここにヘルプメッセージを記述します。
-        このように改行も含めることができます。
+        ■ 判定 (nOT>=x)
+          nD6のダイスロールをして、その合計が x を超えていたら成功。
+          出目6が2個以上あればクリティカル。出目が全て1ならファンブル。
+
+        ■ 表
+        - オンセツール決定表 (TOOLS)
       TEXT
 
       register_prefix()
